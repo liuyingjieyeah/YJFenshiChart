5c066278a3bf1de814631da291e0840adb81606e
liuyingjieyeah <liuyingjieyeah@163.com>
2017-08-18 08:31:02 +0800

    Initial Commit

---
+++ YJFenshiChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJFenshiChart
+//
+//  Created by liuyingjie on 2017/8/18.
+//  Copyright © 2017年 liuyingjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
